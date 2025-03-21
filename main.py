1b2eee4d633d195be0f5e7bd836468ae8f113413
amjad.khan <amjad.khan281989@outlook.com>
2025-03-21 17:01:05 +0530

    add all crud method

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ def index():
 @app.get('/about')
 def about():
     return {'data':'about page'}
+
+@app.get('/blog/id')
+def index_blog_id():
+    return {'data':'blog id'}
+
+@app.get('/blog/{id}')
+def show(id):
+    # fetch blog with id = id
+    return {'data':id}
