faa968748b9ee38e69f307a63fbd3590f78862fe
Amjad Khan <amjad.khan281989@outlook.com>
2025-03-24 00:17:06 +0530

    pull the data

--- all_crud_method/post_get_put_delete.py
+++ all_crud_method/post_get_put_delete.py
@@ -6,15 +6,14 @@ import psycopg2
 app = FastAPI()
 data = []
 
-<<<<<<< HEAD
+
 conn = psycopg2.connect(
-    
-=======
-db_name = "test"
-db_user = "postgres"
-db_pswd = "1234"
-db_host = "localhost"
-db_port = "5432"
+            db_name = "test",
+            db_user = "postgres",
+            db_pswd = "1234",
+            db_host = "localhost",
+            db_port = "5432"
+)
 
 conn = psycopg2.connect(
             dbname = db_name,
@@ -22,7 +21,6 @@ conn = psycopg2.connect(
             password = db_pswd,
             host = db_host,
             port = db_port
->>>>>>> 38db9814fdf519effa126268e3781e62bed05688
 )
 
 class Book(BaseModel):
